cc6c84f90e69154e1ffa7df02355ec2adec7fb28
Ersin <ckmrsn@gmail.com>
2021-04-12 17:20:13 +0300

    initialized
    Context api ile alışveriş speti uygulaması.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState } from 'react'
+import './style.css'
+import { Route } from 'react-router-dom'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import { data } from './data'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const BooksContext = createContext()
+
+export default function App() {
+	const [state, setstate] = useState({
+		booklist: data,
+		cart: [],
+	})
+
+	const addToCart = (book) => {
+		setstate({
+			...state,
+			cart: state.cart.find((cartitem) => cartitem.id === book.id)
+				? state.cart.map((cartitem) =>
+						cartitem.id === book.id
+							? { ...cartitem, count: cartitem.count + 1 }
+							: cartitem
+				  )
+				: [...state.cart, { ...book, count: 1 }],
+		})
+	}
+	const removeFromCart = (id) => {
+		setstate({
+			...state,
+			cart: state.cart.filter((cartitem) => cartitem.id !== id),
+		})
+	}
+
+	const increase = (id) => {
+		setstate({
+			...state,
+			cart: state.cart.map((cartitem) =>
+				cartitem.id === id
+					? { ...cartitem, count: cartitem.count + 1 }
+					: { ...cartitem }
+			),
+		})
+	}
+	const decrease = (id) => {
+		setstate({
+			...state,
+			cart: state.cart.map((cartitem) =>
+				cartitem.id === id
+					? {
+							...cartitem,
+							count:
+								cartitem.count > 1
+									? cartitem.count - 1
+									: cartitem.count,
+					  }
+					: { ...cartitem }
+			),
+		})
+	}
+	return (
+		<BooksContext.Provider
+			value={{ state, addToCart, increase, decrease, removeFromCart }}
+		>
+			<div className='App'>
+				<h1>Alışveriş Sepeti Uygulaması</h1>
+				<Route exact path='/' component={Products} />
+				<Route path='/cart' component={Cart} />
+			</div>
+		</BooksContext.Provider>
+	)
 }
-
-export default App;
